f32a26d033b6bfeb6c199d722147a7f178ce296a
ShivamBunge <shivambunge9898@gmail.com>
2021-09-11 23:00:18 +0530

    last

--- django_project/settings.py
+++ django_project/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-print("hello",SECRET_KEY)
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_VAL')
-print(DEBUG)
+DEBUG = (os.environ.get('DEBUG_VAL')=='True')
+
 
 ALLOWED_HOSTS = ['shivamsdjangoblog.herokuapp.com']
 
